1207e1899847e231c72a70c57090b317ce536d55
Hadia-1396 <hadia.shabbir@devsinc.com>
2024-04-04 13:54:23 -0700

    added crud operations for the user model

---
+++ server/src/users/dto/create-user.dto.ts
@@ -0,0 +1,35 @@
+import { Gender } from 'src/constants/enums/Gender.enum';
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  MaxLength,
+  IsEnum,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MaxLength(15)
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  age: number;
+
+  @IsEnum(Gender, {
+    message: 'Gender must be either male or female',
+  })
+  gender: Gender;
+
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  profile_picture: string;
+}
---
+++ server/src/users/users.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Gender } from 'src/constants/enums/Gender.enum';
+
+@Schema()
+export class Users {
+  @Prop()
+  name: string;
+
+  @Prop()
+  age: number;
+
+  @Prop({ enum: Object.values(Gender), default: Gender.Male })
+  gender: Gender;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  username: string;
+
+  @Prop()
+  profile_picture: string;
+}
+
+export const UsersSchema = SchemaFactory.createForClass(Users);
